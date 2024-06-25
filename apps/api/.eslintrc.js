0c39b9201e1d8e6280b6f0fbf1ec0741db3e45d9
mateusmoov <mateusvasconcelos4210@gmail.com>
2024-06-25 00:20:17 -0300

    chore(api): add type safe

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -5,7 +7,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', '@ts-safeql/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +23,19 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@ts-safeql/check-sql': [
+      'error',
+      {
+        connections: [
+          {
+            connectionUrl: process.env.DATABASE_URL,
+            migrationsDir: './prisma/migrations',
+            targets: [
+              { tag: 'prisma.+($queryRaw|$executeRaw)', transform: '{type}[]' },
+            ],
+          },
+        ],
+      },
+    ],
   },
 };
