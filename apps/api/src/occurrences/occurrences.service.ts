176055ffd88a1811512a5dca4e52538267da1a10
mateusmoov <mateusvasconcelos4210@gmail.com>
2024-07-04 00:27:21 -0300

    refactor(api): split create occurrence method

--- apps/api/src/occurrences/occurrences.service.ts
+++ apps/api/src/occurrences/occurrences.service.ts
@@ -3,7 +3,7 @@ import { CreateOccurrenceDto } from './dto/create-occurrence.dto';
 import { UpdateOccurrenceDto } from './dto/update-occurrence.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Occurrence } from 'src/database/entities/occurrence.entity';
-import { Repository } from 'typeorm';
+import { Point, Repository } from 'typeorm';
 import { Monster } from 'src/database/entities/monster.entity';
 
 @Injectable()
@@ -19,37 +19,58 @@ export class OccurrencesService {
     const { monsterName, dangerLevel, monster, location } = createOccurrenceDto;
 
     try {
-      let existingMonster = await this.monstersRepository.findOne({
-        where: { name: monsterName },
-      });
-
-      if (
-        !existingMonster ||
-        (existingMonster && existingMonster.danger_level !== dangerLevel)
-      ) {
-        const newMonster = this.monstersRepository.create({
-          name: monsterName,
-          image_url: monster.url,
-          description: monster.description,
-          danger_level: dangerLevel,
-        });
-
-        existingMonster = await this.monstersRepository.save(newMonster);
-      }
-
-      const newOccurrence = this.occurrencesRepository.create({
-        monster_id: existingMonster.id,
-        location: {
-          type: 'Point',
-          coordinates: [location[0].lng, location[0].lat],
-        },
-      });
-
-      return this.occurrencesRepository.save(newOccurrence);
+      const existingMonster = await this.findOrCreateMonster(
+        monsterName,
+        dangerLevel,
+        monster,
+      );
+      const newOccurrence = await this.createOccurrence(
+        existingMonster.id,
+        location,
+      );
+      return newOccurrence;
     } catch (error) {
       console.error(error);
       throw new NotFoundException(`Monster with name ${monsterName} not found`);
     }
+  }
+
+  private async findOrCreateMonster(
+    monsterName: string,
+    dangerLevel: string,
+    monster: any,
+  ) {
+    let existingMonster = await this.monstersRepository.findOne({
+      where: { name: monsterName },
+    });
+
+    if (
+      !existingMonster ||
+      (existingMonster && existingMonster.danger_level !== dangerLevel)
+    ) {
+      const newMonster = this.monstersRepository.create({
+        name: monsterName,
+        image_url: monster.url,
+        description: monster.description,
+        danger_level: dangerLevel,
+      });
+
+      existingMonster = await this.monstersRepository.save(newMonster);
+    }
+
+    return existingMonster;
+  }
+
+  private async createOccurrence(monsterId: number, location: Point) {
+    const newOccurrence = this.occurrencesRepository.create({
+      monster_id: monsterId,
+      location: {
+        type: 'Point',
+        coordinates: [location[0].lng, location[0].lat],
+      },
+    });
+
+    return this.occurrencesRepository.save(newOccurrence);
   }
 
   findAll() {
